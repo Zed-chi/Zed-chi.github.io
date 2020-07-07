7ede42c3590642356bbc3a2ab2f224c41e980e8e
AlexE <zed-chi@mail.ru>
2020-07-07 22:26:44 +0300

    2020 upd)

--- Pages/ListEasy/script.js
+++ Pages/ListEasy/script.js
@@ -1,23 +1,95 @@
-var template = function(text) {
-  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+/* GLOBAL SHIT */
+const URL = "http://localhost:3000";
+let AUTH = "";
+
+function template(name, id) {
+    return `
+    <p>
+        <span>${name}</span>
+        <i class="glyphicon glyphicon-remove" id="${id}"></i>
+    </p>`;
 };
 
-var main = function() {
-  $('form').submit(function() {
-      var text=$('#todo').val();
-    	var html=template(text);
-    	$('.list').append(html);
-    	$('#todo').val('');
-    
-    return false;  
-  });
- $('.main').on('click','.glyphicon-star',function(){
- 	$(this).toggleClass('active');
- });
- $('.main').on('click','.glyphicon-remove',function(){
- 	$(this).parent().remove();
- }); 
-  
+function setAuthHeader() {
+    const code = $(".keycode__input").val();
+    if (code) {
+        AUTH = code;
+    }
+}
+
+function fetchWatchItems() {
+    const jqxhr = $.getJSON(`${URL}/films`)
+        .done((data) => {
+            for (var row of data["rows"]) {
+                $(".list").append(template(row["name"], row["id"]));
+            }
+        })
+        .fail((err) => {
+            console.log(err);
+        });
+}
+
+function postWatchItem() {
+    const title = $("#todo").val();
+    if (title && AUTH) {
+        $.ajax({
+            url: `${URL}/films`,
+            headers: {
+                "authorization": AUTH,
+                "Access-Control-Allow-Origin": "*",
+            },
+            type: "POST",
+            method: "post",
+            crossOrigin: true,
+            accept: "application/json",
+            dataType: "json",
+            data: {
+                "name": title,
+            },
+            success: (data) => {
+                $(".list").append(template(title, data["row"]["id"]));
+                $("#todo").val("");
+            },
+        });
+    }
+}
+
+function deleteWatchItem(id, parent) {3
+    console.log("1");
+    if (id && AUTH && parent) {
+        console.log("2");
+        $.ajax({
+            url: `${URL}/film/${id}`,
+            headers: {
+                "authorization": AUTH,
+                "Access-Control-Allow-Origin": "*",
+            },
+            type: "delete",
+            method: "delete",
+            crossOrigin: true,
+            accept: "application/json",
+            dataType: "json",
+            success: (data) => {
+                parent.remove();
+            },
+        });
+    }
+}
+
+function main() {
+    $("form").submit(() => {
+        postWatchItem();
+        return false;
+    });
+    $(".main").on("click", ".glyphicon-star", () => $(this).toggleClass("active"));
+    $(".main").on("click", ".glyphicon-remove", (e) => {        
+        var id = e.target.getAttribute("id");
+        var parent = e.target.parentElement;
+        console.log(id, parent);
+        deleteWatchItem(id, parent);
+    });
+    $(".keycode").on("click", ".keycode__button", setAuthHeader);
+    fetchWatchItems();
 };
 
 $(document).ready(main);
